89381113de8f47cab8ee309537901de6e103b1f2
David Davis <davidjamesdavis.djd@gmail.com>
2018-11-14 22:18:49 -0800

    project init

---
+++ index.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"cloud.google.com/go/firestore"
+)
+// Continue where I left off: https://cloud.google.com/appengine/docs/standard/go/building-app/authenticating-users
+
+func userHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	name := r.Form["name"]
+	email := r.Form["email"]
+
+	_, _, err = client.Collection("users").Add(ctx, map[string]interface{}{
+		"name": name,
+		"email": email,
+	})
+	if err != nil {
+		log.Fatalf("Failed adding new user: %v", err)
+	}
+}
+
+func main() {
+	ctx := context.Background()
+
+	// [START fs_initialize]
+	// Sets your Google Cloud Platform project ID.
+	projectID := "golang-gae-boilerplate"
+
+	// Get a Firestore client.
+	client, err := firestore.NewClient(ctx, projectID)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+
+	// Close client when done.
+	defer client.Close()
+	// [END fs_initialize]
+
+	http.HandleFunc("/api/user", userHandler)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	log.Printf("Listening on port %s", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
--- src/App.js
+++ src/App.js
@@ -3,23 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  handleSubmit(formData) {
+    console.log(formData)
+    // fetch('https://gae.appspot.com/api/user', {
+    //   method: 'post',
+    //   body: JSON.stringify(formData)
+    // })
+    // .catch(err => console.log(err))
+    // .then(res => res.json())
+    // .then(data => {
+    //   console.log(data)
+    // })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" name="name" required placeholder="Name"></input>
+          <input type="email" name="email" required placeholder="Email"></input>
+          <button type="Submit">Submit</button>
+        </form>
       </div>
     );
   }
